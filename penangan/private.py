c05572da1a17ffa34efbb865e20f6abe543da32b
sshlte55 <82154515+sshlte55@users.noreply.github.com>
2021-04-21 07:43:45 +0700

    Update and rename handlers/private.py to penangan/private.py

--- penangan/private.py
+++ penangan/private.py
@@ -9,27 +9,26 @@ from helpers.filters import other_filters2
 async def start(_, message: Message):
     await message.reply_sticker("CAACAgQAAx0CTv65QgABBfJlYF6VCrGMm6OJ23AxHmD6qUSWESsAAhoQAAKm8XEeD5nrjz5IJFYeBA")
     await message.reply_text(
-        f"""**Hey, I'm {bn} 🎵
+        f"""**Hai saya {bn} 
+Saya bisa memutar musik di panggilan suara grup Anda. Dikembangkan oleh [ɢᴏᴏᴅ ʙᴏʏs](https://t.me/GB_03101999).
 
-I can play music in your group's voice call. Developed by [Uvindu Bro 🇱🇰](https://t.me/Uvindu_Bro).
-
-Add me to your group and play music freely!**
+Tambahkan saya ke grup Anda dan mainkan musik dengan bebas!
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "📦 Source Code 🛠", url="https://github.com/UvinduBro/GroupMusicBot")
+                        "📝 Daftar Perintah Bot", url="https://telegra.ph/Ultroid-04-21")
                   ],[
                     InlineKeyboardButton(
-                        "🔥 Group", url="https://t.me/UvinduBr"
+                        "👮🏻‍♀ Assistant Bot", url="https://t.me/GB_Assistant"
                     ),
                     InlineKeyboardButton(
-                        "🔊 Channel", url="https://t.me/UvinduBro"
+                        "👑 pemilik Bot", url="https://t.me/GB_03101999"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "➕ Add To Your Group ➕", url="https://t.me/UBGroupMusicPlayerBot?startgroup=true"
+                        "➕ Tambahkan ke Grup Anda ➕", url="https://t.me/GB_VcMusicBot?startgroup=true"
                     )]
             ]
         ),
@@ -43,10 +42,8 @@ async def gstart(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🔊 Channel", url="https://t.me/UvinduBro")
+                        "👑 Pemilik Bot", url="https://t.me/GB_03101999")
                 ]
             ]
         )
    )
-
-
