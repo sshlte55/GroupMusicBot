f480966b6fa4c2e6385c6fb99e97dceffa9e4118
sshlte55 <82154515+sshlte55@users.noreply.github.com>
2021-04-21 19:30:45 +0700

    Update and rename handlers/songs.py to penangan/songs.py

--- penangan/songs.py
+++ penangan/songs.py
@@ -8,7 +8,7 @@ from youtube_search import YoutubeSearch
 
 def time_to_seconds(time):
     stringt = str(time)
-    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+    return sum(int(x) * 60  i for i, x in enumerate(reversed(stringt.split(':'))))
 
 
 @Client.on_message(filters.command('song') & ~filters.private & ~filters.channel)
@@ -22,7 +22,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Finding the song...')
+    m = message.reply('🔎 Menemukan lagunya...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -45,13 +45,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("📥 Downloading the song by @UvinduBro...")
+    m.edit("📥 Mendownload lagu oleh @GB_03101999...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by @UvinduBro**'
+        rep = '🎵 Diunggah oleh @GB_03101999**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
